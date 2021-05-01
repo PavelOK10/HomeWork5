04c4522d6a0fdd43010e13a63f00dca84341b940
PaRus <PaRusakovich@gmail.com>
2021-05-02 02:58:14 +0300

    Добавьте файлы проекта.

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace HomeWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите высоту треугольника №1: ");
+            int height1 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < height1; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                    Console.WriteLine();
+            }
+
+            Console.WriteLine("Введите высоту треугольника №2: ");
+            int height2 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < height2; i++)
+            {
+                for (int j = height2; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                    Console.WriteLine();
+            }
+
+            Console.WriteLine("Введите высоту треугольника №3: ");
+            int height3 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < height3; i++)
+            {
+
+                for (int j = height3; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int w = 0; w <= i; w++)
+                {
+                    Console.Write("*");
+                }
+                    Console.WriteLine();
+            }
+
+            Console.WriteLine("Введите высоту треугольника №4: ");
+            int height4 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < height4; i++)
+            {
+
+                for (int j = i; j < height4; j++)
+                {
+                     Console.Write("*");
+                }
+                     Console.WriteLine();
+                for (int w = 0; w <= i ; w++)
+                {
+                     Console.Write(" ");
+                }
+            }
+        }
+    }
+}
